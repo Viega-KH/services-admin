5ac61601bb99d9307defe1a561aed1273df9fc21
Viega-KH <viego800000@gmail.com>
2025-03-23 16:08:18 +0500

    db psq change

--- apps/config/settings.py
+++ apps/config/settings.py
@@ -88,23 +88,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'hardtech',  
-#         'USER': 'postgres', 
-#         'PASSWORD': '123456',  
-#         'HOST': 'localhost',  
-#         'PORT': '5432',  
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hardtech',  
+        'USER': 'postgres', 
+        'PASSWORD': '123456',  
+        'HOST': 'localhost',  
+        'PORT': '5432',  
+    }
+}
 
 
 
